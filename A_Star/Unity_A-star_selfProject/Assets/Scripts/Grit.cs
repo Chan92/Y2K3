5e9435ffedd19435c8464014c1c5d61f256b3b32
Chantal <c_voort1992@hotmail.com>
2020-02-14 12:31:30 +0100

    project Startup

---
+++ A_Star/Unity_A-star_selfProject/Assets/Scripts/Grit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grit : MonoBehaviour{
+	[SerializeField]
+	private LayerMask unwalkableMask;
+	[SerializeField]
+	private Vector2 gridWorldSize;
+	[SerializeField]
+	private float nodeRadius;
+
+	private Node[,] grid;
+
+	private void OnDrawGizmos() {
+		Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+	}
+}
